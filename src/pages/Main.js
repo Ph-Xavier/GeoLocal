9e0d0cedccf406453a70ce53694987afaf4a5957
Pedro Xavier <pedro_xavier9@outlook.com>
2026-04-27 19:23:36 -0300

    feat(main): implementar a main

---
+++ src/pages/Main.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import useLocation from "../hooks/useLocation";
+
+export default function Main() {
+  const { coords, errorMsg } = useLocation(); //Hook personalizado para obter a localização
+  if (errorMsg) {
+    return (
+      <View style={styles.center}>
+        <Text style={styles.errorText}>{errorMsg}</Text>
+      </View>
+    );
+  }
+
+  if (!coords) {
+    return (
+      <View style={styles.center}>
+        <ActivityIndicator size="large" color="#0000ff" />
+        <Text style={styles.loadingText}>Carregando localização.....</Text>
+      </View>
+    );
+  }
+
+  return (
+    <MapView
+      style={styles.map}
+      initialRegion={{
+        latitude: coords.latitude,
+        longitude: coords.longitude,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+      }}
+      showsUserLocation={true}
+    >
+      <Marker
+        coordinate={{
+          latitude: coords.latitude,
+          longitude: coords.longitude,
+        }}
+        title="Minha Localização"
+        description="Esta é a minha localização atual"
+      />
+    </MapView>
+  );
+}
+
+const styles = StyleSheet.create({
+  map: {
+    flex: 1,
+  },
+  center: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  loadingText: {
+    marginTop: 10,
+    fontSize: 16,
+    textAlign: "center",
+  },
+  errorText: {
+    fontSize: 16,
+    color: "red",
+    textAlign: "center",
+  },
+});
